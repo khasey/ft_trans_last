68a312ee6c01a7e43b7a060bac9c10f705e6ee72
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-05-01 02:28:24 +0200

    Add files via upload

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -4,12 +4,11 @@ import Ball from'./ball/Ball'
 function Login() {
     return (
     <div className="all">
-        <div className="all_in">
-        {/* <Ball/>  */}
-            <div className="all_in_ball">
-               <div className="all_in_ball_b">
+        <div className="all_b">
                 
-               </div>
+        </div>
+        <div className="all_in">
+            <div className="all_in_ball">
             </div>
             <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-dcaa0fd451e852b7a12bb0d63673e9c73fec244c0afcc75bdfde8ecdab1c1ec7&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fgame&response_type=code" className="all_in_button">
                 <span className='all_in_button_text'>CONNECT-TOI</span>
