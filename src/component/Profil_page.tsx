3452f370661f9739e88ffb0dbcefc37025ca4fb9
Khasey_Nine_Z <92310470+khasey@users.noreply.github.com>
2023-05-05 00:38:16 +0200

    Add files via upload

--- src/component/Profil_page.tsx
+++ src/component/Profil_page.tsx
@@ -3,6 +3,8 @@ import './Profil_page.scss'
 import { Avatar, Box, Button, ButtonGroup, Switch, TextField, Typography, alpha, styled } from '@mui/material'
 import { pink } from '@mui/material/colors';
 
+
+
 const buttons = [
   <Button key="one">Last Matchs</Button>,
   <Button key="two">Last Week</Button>,
@@ -16,6 +18,9 @@ const PinkSwitch = styled(Switch)(({ theme }) => ({
     color: 'white',
     '&:hover': {
       backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
+    },
+    "@media screen and (width < 1000px)":{
+      fontSize:'10px',
     },
   },
   '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
@@ -34,14 +39,26 @@ function Scoreboard() {
 
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" 
           sx={{
+            "@media screen and (width < 1000px)":{
+              width:'60px',
+              height:'60px',
+            },
             width: '80px',
             height: '80px',
             marginLeft: '5%',
-            marginRight:'5%'
+            marginRight:'5%',
           }} />
 
         <TextField
-          sx={{marginRight:'45vw'}}
+          sx={{
+            "@media screen and (width < 1000px)":{
+              marginRight:'3vw',
+            },
+            "@media screen and (width < 1500px)":{
+              marginRight:'10vw',
+            },
+            marginRight:'45vw',
+          }}
           InputLabelProps={{
             style: { color: "white" },
           }}
@@ -63,18 +80,28 @@ function Scoreboard() {
           id="outlined-controlled"
           label="USERNAME"
           value={name}
+          size='medium'
           onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
           setName(event.target.value);
         }}
       />
 
           <Typography variant="h6" gutterBottom sx={{
+            "@media screen and (width < 1000px)":{
+              fontSize:'15px',
+              margin:'10px',
+            },
+            "@media screen and (width < 1500px) and (width > 1000px)":{
+              fontSize:'20px',
+            },
             margin:'0',
             color:'white',
           }}>
              Enable 2fa
           </Typography>
-          <PinkSwitch defaultChecked sx={{margin:'0'}} />
+          <PinkSwitch defaultChecked sx={{
+            margin:'10px'
+            }} />
           
 
         </div>
@@ -110,7 +137,18 @@ function Scoreboard() {
           </div>
         </div>
         <div className="all_score_ladder">
+              <div className="all_score_ladder_logo">
+                <p className="all_score_ladder_logo_lvl">1</p>              
+                <img src="./image/lvl2.png" alt="" className="all_score_ladder_logo_img" />
+              </div>
+              <Box className="all_score_ladder_exp">
+                <p className="all_score_ladder_exp_text">
+                  YOUR EXP:
+                </p>
+                <div className="all_score_ladder_exp_bar">
 
+                </div>
+              </Box>
         </div>
       </div>
     </div>
